064a1c801b4037ebf86df105343fdb5fa0806d0c
Amit Kumar <akumar27@sapient.com>
2018-05-13 10:59:44 -0400

    port changed

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/hi', (req, res) => res.send('Hello Babita!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3002, () => console.log('Example app listening on port 3000!'))
